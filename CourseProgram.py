f830d6d656418bf1252e63d94ce5022e54936625
Mohan Korapala <mohan_korapala1@baylor.edu>
2022-03-02 15:27:12 -0600

    Midterm 3/2

--- CourseProgram.py
+++ CourseProgram.py
@@ -8,7 +8,8 @@ You have been given partial code. The objective is to reproduce the output as sh
    and the output should reflect that as shown in the picture.
 '''
 
-
+from tracemalloc import stop
+import CourseClass as cc
 
 def main():
 
@@ -17,7 +18,31 @@ def main():
     seats = 4
     status = 'open'
     students = ['John','James','Jill','Jack','Joanne']
+    
+    course = cc.Course(name,crn,seats,status)
+    for rec in students:
+       rec_reg = cc.Register(rec,crn)
+       print()
+       print("Student Name:",rec_reg.get_name())
+       print("Course Name:",course.get_name())
+       print("CRN:",course.get_crn())
+       seats -= 1
+       if seats >=0:
+         print("Seats Left:", int(seats))
+         print()
+       else:
+         print('Sorry', rec_reg.get_name(), 'registrations is closed for', course.get_name())
 
+       
+       
+       
+       
+       
+
+      
+      
+      
+      
     
 main()
 
